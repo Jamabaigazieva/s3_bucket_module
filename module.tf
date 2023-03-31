dc8d35123febaefdca0025d70f3c01b3f689cdbe
Jamabaigazieva <baigazieva@gmail.com>
2023-03-31 10:27:11 +0600

    updated s3

---
+++ module.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl = "private"
+  tags = {
+    "managed-by" = "terraform"
+  }
+}
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id = "my_rule"
+
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+    status = "Enabled"
+  }
+}
+
